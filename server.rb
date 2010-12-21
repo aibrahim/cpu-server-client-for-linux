b550d83ee9797efa2807736e6ee8dee5abd05b6c
Abdullah <abdullahibra@gmail.com>
2010-12-21 13:33:51 +0200

    the server

---
+++ server.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+require 'socket'               			# Get sockets from stdlib
+
+server = TCPServer.open(2000)  			# Socket to listen on port 2000
+loop {                         			# Servers run forever
+  Thread.start(server.accept) do |client|       # Wait for a client to connect
+  	client.puts(Time.now.ctime)  			# Send the time to the client
+  		client.puts "Closing the connection. Bye!"
+  	client.close					# Disconnect from the client
+ end
+}	
